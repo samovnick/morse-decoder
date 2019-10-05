43f5225930be8823a55c7850e80c1b65cb479589
Nickolay Samovendyuk <samovnick@gmail.com>
2019-10-05 19:30:18 +0300

    morse-decoder feat: implement task

--- src/index.js
+++ src/index.js
@@ -39,7 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-    return '*';
+    let str = '';
+    let chars = [];
+    let i = 0;
+    while (i < expr.length) {
+        chars.push(expr.slice(i, i + 10));
+        i += 10;
+    };
+    // console.dir(chars);
+    let letters = chars.map(element => {
+        let char = "";
+        let index = element.indexOf('1') + 1;
+        if (index) {
+            let letter = "";
+            if (index % 2 == 0) char = element.slice(index);
+            else char = element.slice(index - 1);
+            for(let i = 0; i < char.length; i += 2) {
+                if (char[i + 1] == "1") letter += "-";
+                else letter += "."
+            };
+            return MORSE_TABLE[letter];
+        } else return " ";
+
+        return index;
+    });
+
+    // console.dir(letters);
+
+    return letters.join("");
 }
 
 module.exports = {
